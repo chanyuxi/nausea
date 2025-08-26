407a61053b6ed5e1b61489f96832afb4d3e59f7d
陈永政 <2438149743@qq.com>
2025-08-26 17:30:45 +0800

    chore: update the building process

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,79 +1,107 @@
 import { fileURLToPath } from 'node:url'
-import { resolve } from 'node:path'
-import { copyFileSync, existsSync, mkdirSync, readdirSync } from 'node:fs'
+import { resolve, dirname } from 'node:path'
+import { stat, copyFile, mkdir, readdir } from 'node:fs/promises'
 
 import type { TaskFunctionCallback } from 'gulp'
 import { series, task } from 'gulp'
 
-import { rimrafSync } from 'rimraf'
-import { globSync } from 'glob'
-import logger from 'gulplog'
+import { rimraf } from 'rimraf'
+import { glob } from 'glob'
 
+import type { Plugin, ModuleFormat } from 'rollup'
 import { rollup } from 'rollup'
 import typescript from '@rollup/plugin-typescript'
 
-const dirname = fileURLToPath(new URL('.', import.meta.url))
+const root = fileURLToPath(new URL('.', import.meta.url))
 
-function dirnameResovle(...args: string[]) {
-    return resolve(dirname, ...args)
+function rootResovle(...args: string[]) {
+	return resolve(root, ...args)
 }
 
-function clean(cb: TaskFunctionCallback) {
-    rimrafSync(dirnameResovle('dist'))
-    cb()
+async function clean(cb: TaskFunctionCallback) {
+	await rimraf(rootResovle('dist'))
+	cb()
 }
 
-function copyScssFiles(cb: TaskFunctionCallback) {
-    const dir = dirnameResovle('dist/styles')
-    if (!existsSync(dir)) {
-        mkdirSync(dir, { recursive: true })
-    }
-    const styleFiles = globSync('src/styles/*.scss')
-    logger.info(`found ${styleFiles.length} style files.`)
-    styleFiles.forEach(file => {
-        copyFileSync(file, file.replace('src', 'dist'))
-    })
-    logger.info(`copied.`)
-    cb()
+async function copyScssFiles() {
+	const getDest = (file: string) => file.replace('src', 'dist')
+	const files = await glob('src/styles/**/*.scss')
+	await Promise.all(
+		files.map(file => safeCopy(file, getDest))
+	);
+}
+
+async function safeCopy(src: string, getDest: (file: string) => string) {
+	const dest = getDest(src)
+	const destDir = dirname(dest)
+	await stat(destDir).catch(async () => {
+		await mkdir(destDir, { recursive: true })
+	})
+	await copyFile(src, dest)
+}
+
+async function getComponentEntries() {
+	const componentsDir = rootResovle('src/components')
+	const entries: Record<string, string> = {}
+
+	const dirs = await readdir(componentsDir)
+	await Promise.all(
+		dirs.map(async (dir) => {
+			const entry = resolve(componentsDir, dir, 'index.tsx')
+			try {
+				await stat(entry)
+				entries[dir] = entry
+			} catch (error) {
+			}
+		})
+	)
+
+	return entries
+}
+
+function isExternal(id: string) {
+	return id.startsWith('react')
+}
+
+function getPlugins(format: ModuleFormat) {
+	const plugins: Plugin[] = [
+		typescript()
+	]
+	if (format === 'es') {
+	}
+	return plugins
+}
+
+async function buildImpl(input: Record<string, string>, format: ModuleFormat) {
+	const bundle = await rollup({
+		input,
+		external: isExternal,
+		plugins: getPlugins(format),
+	})
+	await bundle.write({
+		dir: rootResovle(`dist/${format}`),
+		format,
+	})
+}
+
+async function buildDts(input: Record<string, string>) {
 }
 
 async function build(cb: TaskFunctionCallback) {
-    logger.info('build by rollup')
-    const plugins = [
-        typescript()
-    ]
+	const componentEntries = await getComponentEntries()
+	const input = {
+		...componentEntries,
+		index: rootResovle('src/index.ts'),
+	}
 
-    const input = {
-        ...getComponentEntries(),
-        index: dirnameResovle('src/index.ts'),
-    }
-    // We only produce es mode for now, and we will consider other targets later.
-    const bundle = await rollup({
-        input,
-        plugins,
-    })
-    await bundle.write({
-        dir: dirnameResovle('dist/es'),
-        format: 'es',
-    })
-    cb()
-}
-
-function getComponentEntries() {
-    const componentsDir = dirnameResovle('src/components')
-    const entries: Record<string, string> = {}
-    readdirSync(componentsDir).reduce((acc, dir) => {
-        const entry = resolve(componentsDir, dir, 'index.tsx')
-        if (existsSync(entry)) {
-            acc[dir] = entry
-        }
-        return acc
-    }, entries)
-    return entries
+	await Promise.all([
+		...[copyScssFiles(), buildImpl(input, 'es')],
+		buildDts(input),
+	])
+	cb()
 }
 
 task('build', series(
-    clean,
-    copyScssFiles,
-    build,
+	clean,
+	build,
 ))
